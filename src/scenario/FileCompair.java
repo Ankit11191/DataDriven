16f2999f338895a0cab09305b2e930b0f829eeb9
Ankit11191 <mittal.ankit91@gmail.com>
2017-09-18 13:42:14 +0530

    changes

--- src/scenario/FileCompair.java
+++ src/scenario/FileCompair.java
@@ -6,5 +6,6 @@ public class FileCompair {
 	public static void main(String[] args) {
 		System.out.println(SetDBconnection.getStatement());
 		SetDBconnection.Readgetdata(ReadProerties.propsObjectsSplit("Query"));
+		System.out.println("ankit");
 	}
 }
