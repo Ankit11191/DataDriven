78a27c4914d22fa89145dc7ed8725a0457a87ffc
Ankit11191 <mittal.ankit91@gmail.com>
2017-10-02 21:44:59 +0530

    Added code for write into excel sheet as user prospective

---
+++ src/scenario/WriteIntoExcelSheet.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package scenario;
+
+/**
+ * @author Ankit.Mittal
+ *
+ */
+public class WriteIntoExcelSheet {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
