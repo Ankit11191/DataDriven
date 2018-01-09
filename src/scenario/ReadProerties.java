67146e01c1165afa43de89dad4887f9494b88be4
Ankit mittal <mittal.ankit91@gmail.com>
2018-01-09 18:50:01 +0530

    Add files via upload

--- src/scenario/ReadProerties.java
+++ src/scenario/ReadProerties.java
@@ -1,32 +1,17 @@
-package scenario;
+package com.cucumber.framework.configreader;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.net.URL;
 import java.util.Properties;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
-
-public class ReadProerties {
-	private static Properties pageElementProperties = new Properties();
-	public static final Logger logger=Logger.getLogger(ReadProerties.class);
-	// ThreadLocal class in Java enables you to create variables that can only
-	// be read and written by the same thread. Thus, even if two threads are
-	// executing the same code, and the code has a reference to a ThreadLocal
-	// variable, then the two threads cannot see each other's ThreadLocal
-	// variables
-	@SuppressWarnings("rawtypes")
-	private static ThreadLocal threadLocalFileName = new ThreadLocal();
-
-	@SuppressWarnings("unchecked")
+public class ReadProerties  {
+	static Properties pageElementProperties = new Properties();
 	private static Properties getObjectRepository(String propfilepath)
 	{
-		PropertyConfigurator.configure("Log4j.properties");
 		try{
-		URL url = ReadProerties.class.getClassLoader()
-				.getResource(propfilepath);
-		//Uniform Resource Identifier (URI) 
+		URL url = ReadProerties.class.getClassLoader().getResource(propfilepath);
+
 		File fileinfolder = new File(url.toURI());
 		String[] filesinDir = fileinfolder.list();
 
@@ -37,10 +22,7 @@ public class ReadProerties {
 				if (!fname.contains(".")) {
 					getObjectRepository(propfilepath + "/" + fname);
 				} else if (fname.endsWith(".properties")) {
-					threadLocalFileName.set(fname);
-					fname = fileinfolder + "\\" + fname;
-					pageElementProperties.load(new FileInputStream(new File(fname)));					
-					logger.info("Property file "+fname+" has loaded");
+					pageElementProperties.load(new FileInputStream(new File(fileinfolder + "/" + fname)));	
 				}
 			}
 		}
@@ -48,17 +30,34 @@ public class ReadProerties {
 	{e.getStackTrace();
 		}
 		return pageElementProperties;
-
 	}
+	
+	public static String[] propsObjectsSplit(String objectname, int totalSeparate)
+	 {		
+			return getObjectRepository("\\resources").getProperty(objectname).split("=",totalSeparate);
+	 }
 	public static String[] propsObjectsSplit(String objectname)
-	 {
-		int lng = getObjectRepository("\\Objects").getProperty(objectname)
-				.split("=").length;
-		// use ternary operator to initialize
-		int aa = (lng > 2) ? 2 : lng;
-
-		String[] values = getObjectRepository("\\Objects").getProperty(
-				objectname).split("=", aa);
-		return values;
+	 {		
+		try
+		{
+			return getObjectRepository("\\resources").getProperty(objectname).split("=");
+		}
+		catch(NullPointerException e)
+        {
+			return null;
+        }
+	 }
+	public static void main(String[] args) {
+		if(propsObjectsSplit("username")!= null)
+		{
+			for(String aaa:propsObjectsSplit("username")) 
+			{
+				System.out.println(aaa);
+			}
+		}
+		else
+		{
+			System.out.println("Please enter correct object key");
+		}
 	}
 }
